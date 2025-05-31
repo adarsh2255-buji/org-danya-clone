b99f27b5ccdcaa49b4a71c7b338d3d5622578874
Dhanya <dhanya@dell.com>
2025-05-31 17:33:00 +0530

    added links in Social icons

--- Trackpi_Demo_org/frontend/src/components/SocialIcon.jsx
+++ Trackpi_Demo_org/frontend/src/components/SocialIcon.jsx
@@ -13,7 +13,13 @@ const SocialIcon=()=> {
   <div className="flex items-center gap-[16px]">
     {/* Icon */}
     <div className="w-[50px] h-[50px] ">
+         <a
+      href="https://www.facebook.com/profile.php?id=61565947096778"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img src={FBIcon} alt="Facebook" className="w-full h-full object-cover" />
+      </a>
     </div>
     {/* Divider */}
     <div className="h-[50px] w-[1px] bg-white opacity-25 mx-[10px]"></div>
@@ -24,7 +30,13 @@ const SocialIcon=()=> {
 {/* Youtube */}
   <div className="flex items-center gap-[16px]">
     <div className="w-[50px] h-[50px]">
+         <a
+      href="https://www.youtube.com/@trackpi"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img src={YoutubeIcon} alt="Youtube" className="w-full h-full object-cover" />
+      </a>
     </div>
     <div className="h-[50px] w-[1px] bg-white opacity-25 mx-[10px]"></div>
   </div>
@@ -34,7 +46,13 @@ const SocialIcon=()=> {
 {/* instagram */}
   <div className="flex items-center gap-[16px]">
     <div className="w-[50px] h-[50px]">
+           <a
+      href="https://www.instagram.com/trackpi_official?igsh=YmwyaHpzYXBueWJz"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img src={InstaIcon} alt="Youtube" className="w-full h-full object-cover" />
+      </a>
     </div>
     <div className="h-[50px] w-[1px] bg-white opacity-25 mx-[10px]"></div>
   </div>
@@ -43,7 +61,13 @@ const SocialIcon=()=> {
 
   <div className="flex items-center gap-[16px]">
     <div className="w-[50px] h-[50px]">
+            <a
+      href="https://medium.com/@trackpi"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img src={MediumIcon} alt="Youtube" className="w-full h-full object-cover" />
+      </a>
     </div>
     <div className="h-[50px] w-[1px] bg-white opacity-25 mx-[10px]"></div>
   </div>
@@ -52,7 +76,13 @@ const SocialIcon=()=> {
 {/* linkdln */}
   <div className="flex items-center gap-[16px]">
     <div className="w-[50px] h-[50px]">
+             <a
+      href="https://www.linkedin.com/company/trackpi-private-limited/?viewAsMember=true"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img src={LinkdlnIcon} alt="Youtube" className="w-full h-full object-cover" />
+      </a>
     </div>
     <div className="h-[50px] w-[1px] bg-white opacity-25 mx-[10px]"></div>
   </div>
@@ -60,7 +90,13 @@ const SocialIcon=()=> {
 {/* Quora */}
   <div className="flex items-center gap-[16px]">
     <div className="w-[50px] h-[50px]">
+               <a
+      href="https://www.linkedin.com/company/trackpi-private-limited/?viewAsMember=true"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img src={QuoraIcon} alt="Youtube" className="w-full h-full object-cover" />
+      </a>
     </div>
     <div className="h-[50px] w-[1px] bg-white opacity-25 mx-[10px]"></div>
   </div>
@@ -68,7 +104,13 @@ const SocialIcon=()=> {
 {/* blogger */}
   <div className="flex items-center gap-[16px]">
     <div className="w-[50px] h-[50px]">
+                 <a
+      href="https://trackpi.blogspot.com/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
       <img src={BloggerIcon} alt="Youtube" className="w-full h-full object-cover" />
+      </a>
     </div>
     <div className="h-[50px] w-[1px] bg-white opacity-25 mx-[10px]"></div>
   </div>
