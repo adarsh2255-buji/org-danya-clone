071e07ad83874917a257a9f714954e09ffa04694
Dhanya <dhanya@dell.com>
2025-05-31 11:56:37 +0530

    increased header logo width

--- Trackpi_Demo_org/frontend/src/components/Header.jsx
+++ Trackpi_Demo_org/frontend/src/components/Header.jsx
@@ -1,31 +1,31 @@
 
 import React from 'react';
 import logo from "../assets/trackpiLogo.png";
-import Background from './Background';
+
 
 const Header = () => {
   return (
     
     <header
     
-      className="w-full h-10 flex items-center justify-between  border-b border-[#5C5C5C] py-2 px-12 relative overflow-hidden"
+      className="w-full h-16 flex justify-between  border-b border-[#5C5C5C] py-2 px-6 relative overflow-hidden"
       style={{
         bottom:"1px"
      
       }}
     >
       {/* Logo */}
-      <div className="flex items-center z-10 relative">
+      <div className="flex items-center  relative">
         <img 
           src={logo}
           alt="Trackpi Logo" 
-          className="w-20 h-12"
+          className="w-45 h-22 -ml-8"
         />
       </div>
 
       {/* Logout */}
-      <div className="flex items-center gap-[40px] z-10 relative">
-        <span className="text-white text-[12px] leading-none">Logout</span>
+      <div className="flex items-center gap-[40px] z-10 relative ">
+        <span className="text-white text-[20px] w-[50px] h-[19px] font-normal leading-none">Logout</span>
       </div>
     </header>
   );
